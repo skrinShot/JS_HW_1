45b963fd3f296ddeaa20a64d2ff0ae28c6289505
Dima <dima.chornous@gmail.com>
2023-08-15 21:19:04 -0600

    Done

---
+++ JS_Lesson_1/main.js
@@ -0,0 +1,57 @@
+let a1 = 'hello';
+console.log(a1)
+
+let a2 = 'owu';
+console.log(a2)
+
+let a3 = 'ua';
+console.log(a3)
+
+let a4 = 1;
+console.log(a4)
+
+let a5 = 10;
+console.log(a5)
+
+let a6 = -999;
+console.log(a6)
+
+let a7 = '123';
+console.log(a7)
+
+let a8 = 3.14;
+console.log(a8)
+
+let a9 = 2.7;
+console.log(a9)
+
+let a10 = 16;
+console.log(a10)
+
+let a11 = true;
+console.log(a11)
+
+let a12 = false;
+console.log(a12)
+
+let first_name;
+first_name = 'Chornous'
+
+let middle_name;
+middle_name = 'Dmytro'
+
+let last_name;
+last_name = 'Romanovich'
+
+console.log(first_name, middle_name, last_name);
+
+let a = 100;
+console.log(typeof a)
+
+let b = '100';
+console.log(typeof b)
+
+let c = true;
+console.log(typeof c)
+
+
