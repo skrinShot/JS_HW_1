2d53ff635173fc2dc2f3cbddc6033886ad1dd049
Dima <dima.chornous@gmail.com>
2023-09-12 19:14:07 -0600

    Done

---
+++ Lesson_9_Dom/HW_9.js
@@ -0,0 +1,114 @@
+//- створити блок,
+// - додати йому класи wrap, collapse, alpha, beta
+// - додати стилі(довільні значення) : колір фону, колір тексту, розмір тексту
+// - додати цей блок в body.
+// - клонувати його повністю, та додати клон в body.
+
+let block = document.createElement('div')
+
+block.classList.add('wrap', 'collapse', 'alpha', 'beta');
+
+block.style.backgroundColor = 'yellow';
+
+document.body.appendChild(block);
+
+let cloneBlock = block.cloneNode(true);
+
+cloneBlock.id = 'cloneBlock';
+
+document.body.appendChild(cloneBlock);
+
+
+// - Є масив:
+// ['Main','Products','About us','Contacts']
+// Зробити ul в середині якої будуть лежати елементи масиву (кожен в своєму li)
+
+
+
+let ulElement = document.createElement('ul');
+
+let arr = ['Main','Products','About us','Contacts'];
+
+arr.forEach(function (itemText) {
+    let liElement = document.createElement('li');
+    liElement.textContent = itemText;
+    ulElement.appendChild(liElement);
+})
+
+document.body.appendChild(ulElement);
+
+// - Є масив
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+// Для кожного елементу масиву зробити блок в якому вивести інформацію про title та monthDuration
+// Завдання робити через цикли.
+
+
+let courses = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+for (const cours of courses) {
+    let div = document.createElement('div');
+    div.classList.add('cours');
+
+    let h2 = document.createElement('h2');
+    h2.innerText = `title - ${cours.title}. monthDuration - ${cours.monthDuration}`;
+    div.append(h2)
+    document.body.appendChild(div);
+
+}
+
+// =========================
+//
+// - Є масив
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+//
+//
+// За допомоги скріпта для кожного елементу масиву зробити <div class='item'> ,
+// в якому буде <h1 class='heading'>  з title  елементу, та <p class='description'>
+// з monthDuration елементу.
+// Завдання робити через цикли.
+
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+
+//
+// ==========================
+//
+// - є масив simpsons, котрий лежить в arrays.js (на цей момент ви вже знаєте де він знаходиться)
+// Проітерувати його, створиши для кожного об'єкту  масиву <div class='member'> та наповнити його данними з об'єкту.
+// Якщо людською мовою: під кожного члена родини зробити блок та наповнити його інформацією з цього об'єкту
+//
+// =========================
+// Цикл в циклі
+// - Є масив coursesArray котрий лежить в arrays.js (на цей момент ви вже знаєте де він знаходиться)
+//
+// Створити для кожного елементу масиву свій блок, блок розділити блоками, в яких будуть зберігатись значення окремих властивостей, для властивості modules зробити список з елементами
+// Приклад структири знаходиться у файлі example.png який лежить в папці з поточним фйлом
+// ------------------
